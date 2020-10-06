6b7525bcb60e78bf18a1173c2e40fe73c7f1d5ba
ngoctuan2406 <49416462+ngoctuan2406@users.noreply.github.com>
2020-10-07 06:35:26 +0700

    test

--- service-worker.js
+++ service-worker.js
@@ -37,3 +37,30 @@ workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/d
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('fox-store').then(function(cache) {
+      return cache.addAll([
+        '/test/',
+        '/test/index.html',
+        '/test/index.js',
+        '/test/style.css',
+        '/test/images/fox1.jpg',
+        '/test/images/fox2.jpg',
+        '/test/images/fox3.jpg',
+        '/test/images/fox4.jpg'
+      ]);
+    })
+  );
+ });
+ 
+ self.addEventListener('fetch', function(e) {
+   console.log(e.request.url);
+   e.respondWith(
+     caches.match(e.request).then(function(response) {
+       return response || fetch(e.request);
+     })
+   );
+ });
+ 
