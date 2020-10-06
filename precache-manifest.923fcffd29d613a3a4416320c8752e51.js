c3348d29b795f2d82688f8b84b7829ec36c11dae
ngoctuan2406 <49416462+ngoctuan2406@users.noreply.github.com>
2020-10-07 06:05:55 +0700

    Updates

---
+++ precache-manifest.923fcffd29d613a3a4416320c8752e51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553f9f5aaffcc42b47f2a95fa62c3e75",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "bcc0450bfabb38ce9096",
+    "url": "/deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb43c5dd6ae9c5788a7b",
+    "url": "/deploy/static/js/2.da3882de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy/static/js/2.da3882de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc0450bfabb38ce9096",
+    "url": "/deploy/static/js/main.44c94f49.chunk.js"
+  },
+  {
+    "revision": "2877f3a407a81b5bcf24",
+    "url": "/deploy/static/js/runtime-main.b97e59d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
